cdd234c4bdc2ac089a429b325142771602e64903
sahland <sahland@mail.ru>
2024-09-03 20:05:35 +0300

    BakeBudget: settings up colors theme for app

---
+++ lib/uikit/colors/color_palette.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+abstract class LightColorPalette {
+  static const white = Colors.white;
+
+  static const black = Colors.black;
+
+  static const lightScaffold = Color(0xFFF9F2DA);
+
+  static const lightPink = Color(0xFFFF7269);
+
+  static const chocoBrown = Color(0xFF93612A);
+
+  static const lightChokoBrown = Color(0xFFB9914B);
+
+  static const textGrey = Color(0xFF686F82);
+}
+
+abstract class DarkColorPalette {
+  static const white = Colors.white;
+
+  static const black = Colors.black;
+
+  static const lightPink = Color(0xFFFF7269);
+
+  static const darkGreen = Color(0xFF16683B);
+
+  static const darkScaffold = Color(0xFF2C2F38);
+
+  static const textGrey = Color(0xFF686F82);
+
+  static const darkGrey = Color(0xFF252830);
+
+  static const blackBanner = Color(0xFF353842);
+}
