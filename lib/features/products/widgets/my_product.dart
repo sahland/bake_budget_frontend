92c84f683fe65a052cd6c6946ff49ca9264e1be2
sahland <sahland@mail.ru>
2024-09-05 20:24:20 +0300

    BakeBudget: update products screen

--- lib/features/products/widgets/my_product.dart
+++ lib/features/products/widgets/my_product.dart
@@ -21,55 +21,67 @@ class MyProduct extends StatelessWidget {
         color: Colors.white,
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.3),
-            spreadRadius: 2,
-            blurRadius: 10,
-            offset: const Offset(0, 5),
-          ),
+          _widgetShadow(),
         ],
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
-              child: FadeInImage.assetNetwork(
-                placeholder: './assets/images/placeholder.png',
-                image: imagePath,
-                fit: BoxFit.cover,
-                width: double.infinity,
-                height: double.infinity,
-                fadeInDuration: const Duration(milliseconds: 300),
-                fadeOutDuration: const Duration(milliseconds: 300),
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 12.0),
-            decoration: BoxDecoration(
-              color: theme.dialogBackgroundColor, // Цвет фона для текста
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-            child: Text(
-              productName,
-              textAlign: TextAlign.center,
-              style: GoogleFonts.poppins(
-                color: Colors.white, // Цвет текста
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
+          _productImage(),
+          _productName(theme),
         ],
       ),
+    );
+  }
+
+  Container _productName(ThemeData theme) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      decoration: BoxDecoration(
+        color: theme.dialogBackgroundColor, // Цвет фона для текста
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+      ),
+      child: Text(
+        productName,
+        textAlign: TextAlign.center,
+        style: GoogleFonts.poppins(
+          color: Colors.white, // Цвет текста
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+
+  Expanded _productImage() {
+    return Expanded(
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        child: FadeInImage.assetNetwork(
+          placeholder: './assets/images/placeholder.png',
+          image: imagePath,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+          fadeInDuration: const Duration(milliseconds: 300),
+          fadeOutDuration: const Duration(milliseconds: 300),
+        ),
+      ),
+    );
+  }
+
+  BoxShadow _widgetShadow() {
+    return BoxShadow(
+      color: Colors.black.withOpacity(0.3),
+      spreadRadius: 2,
+      blurRadius: 10,
+      offset: const Offset(0, 5),
     );
   }
 }
