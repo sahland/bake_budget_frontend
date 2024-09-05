eed7c3fef70bf8f11f7dbf2832291d0ea765061c
sahland <sahland@mail.ru>
2024-09-05 20:24:05 +0300

    BakeBudget: update ingredients screen

--- lib/features/ingredients/view/ingredients_screen.dart
+++ lib/features/ingredients/view/ingredients_screen.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:bake_budget_frontend/features/ingredients/widgets/widgets.dart';
 import 'package:bake_budget_frontend/uikit/uikit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 @RoutePage()
 class IngredientsScreen extends StatefulWidget {
@@ -30,16 +31,33 @@ class _IngredientsScreenState extends State<IngredientsScreen> {
               theme: theme,
               title: 'Ингредиенты',
             ),
-            leading: AppBarReorder(
-              context: context,
-              theme: theme,
-            ),
+            actions: [notifications(theme)],
           ),
           SliverList.builder(
-            itemBuilder: (context, index) => const UserIngredientInfo(),
+            itemBuilder: (context, index) => const UserIngredientInfo(
+              ingredientName: 'молоко',
+              ingredientWeight: 1000,
+              ingredientPrice: 60,
+            ),
           )
         ],
       ),
+    );
+  }
+
+  Stack notifications(ThemeData theme) {
+    return Stack(
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            './assets/icons/plus.svg',
+            width: 20,
+            height: 20,
+            color: theme.secondaryHeaderColor,
+          ),
+        )
+      ],
     );
   }
 }
