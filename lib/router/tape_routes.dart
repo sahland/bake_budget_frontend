3db6814bcb994bbbc5e9b2f704c3a56a8cbcdf94
sahland <sahland@mail.ru>
2024-09-07 20:48:19 +0300

    BakeBudget: update router

--- lib/router/tape_routes.dart
+++
@@ -1,8 +0,0 @@
-import 'package:auto_route/auto_route.dart';
-import 'package:bake_budget_frontend/router/router.dart';
-
-class TapeRoutes {
-  static final routes = AutoRoute(page: TapeWrapperRoute.page, children: [
-    AutoRoute(page: ProductsRoute.page, initial: true),
-  ]);
-}
