ef2c8448f09e31c0c2e9f15cd57a513c2cdeb9d6
sahland <sahland@mail.ru>
2024-09-08 22:33:36 +0300

    BakeBudget: update color palette

--- lib/uikit/colors/color_palette.dart
+++ lib/uikit/colors/color_palette.dart
@@ -21,15 +21,29 @@ abstract class DarkColorPalette {
 
   static const black = Colors.black;
 
-  static const darkScaffold = Color(0xFF2E2527);
+  // static const darkScaffold = Color(0xFF55286F);//scaffold
 
-  static const blueberry = Color(0xFF5CA1E2);
+  // static const blueberry = Color(0xFFDC96E6);//primary
 
-  static const darkBrown = Color(0xFF24171A);
+  // static const darkBrown = Color(0xFF210B2C);//app_bar
+
+  // static const textGrey = Color(0xFF686F82);
+
+  // static const darkPurple = Color(0xFFAE759F);//dialog
+
+  // static const blackBanner = Color(0xFF353842);
+  // //--------------------------------------------------
+
+  static const darkScaffold = Color(0xFF554D59);//scaffold
+
+  static const blueberry = Color(0xFFBC2F2D);//primary
+
+  static const darkBrown = Color(0xFF323739);//app_bar
 
   static const textGrey = Color(0xFF686F82);
 
-  static const darkPurple = Color(0xFF3D373D);
+  static const darkPurple = Color(0xFF7A7287);//dialog
 
   static const blackBanner = Color(0xFF353842);
+  //--------------------------------------------------
 }
