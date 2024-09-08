a896d9331086dd2bbbd8acea907dca9e456bd568
sahland <sahland@mail.ru>
2024-09-08 22:30:20 +0300

    BakeBudget: add update cost dialog for costs screen

---
+++ lib/features/costs/widgets/update_cost_dialog.dart
@@ -0,0 +1,63 @@
+import 'package:bake_budget_frontend/uikit/uikit.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class UpdateCostDialog extends StatefulWidget {
+  final String costName;
+  final int costPrice;
+
+  const UpdateCostDialog({
+    super.key,
+    required this.costName,
+    required this.costPrice,
+  });
+
+  @override
+  State<UpdateCostDialog> createState() => _UpdateCostDialogState();
+}
+
+class _UpdateCostDialogState extends State<UpdateCostDialog> {
+  @override
+  Widget build(BuildContext context) {
+    const saveTitle = 'Сохранить';
+    const deleteTitle = 'Удалить';
+    final theme = Theme.of(context);
+
+    return Dialog(
+      backgroundColor: theme.cardColor,
+      insetPadding: const EdgeInsets.symmetric(vertical: 100, horizontal: 20),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                widget.costName,
+                style: GoogleFonts.pacifico(
+                  color: theme.secondaryHeaderColor,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(height: 20),
+              BaseInputField(title: widget.costName),
+              BaseInputField(title: '${widget.costPrice} руб.'),
+              const SizedBox(height: 20),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  BaseElevatedButton(title: saveTitle),
+                   SizedBox(
+                    width: 10
+                  ),
+                  BaseElevatedButton(title: deleteTitle),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
