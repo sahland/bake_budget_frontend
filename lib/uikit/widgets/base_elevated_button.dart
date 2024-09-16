2a03eab94895fb6560f3c0057558b75cd52c9682
sahland <sahland@mail.ru>
2024-09-16 21:11:33 +0300

    BakeBudget: refactoring base elevated button widget

--- lib/uikit/widgets/base_elevated_button.dart
+++ lib/uikit/widgets/base_elevated_button.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 
 class BaseElevatedButton extends StatefulWidget {
   final String title;
+
+  final double fontSize;
   final VoidCallback? onPressed;
+
   const BaseElevatedButton({
-    super.key,
     required this.title,
+    this.fontSize = 16,
     this.onPressed,
+    super.key,
   });
 
   @override
@@ -16,17 +20,21 @@ class BaseElevatedButton extends StatefulWidget {
 class _BaseElevatedButtonState extends State<BaseElevatedButton> {
   @override
   Widget build(BuildContext context) {
+    const elevation = 10.0;
+
     final theme = Theme.of(context);
 
     return ElevatedButton(
       onPressed: widget.onPressed ?? () {},
       style: ElevatedButton.styleFrom(
-          backgroundColor: theme.primaryColor, elevation: 10),
+        backgroundColor: theme.primaryColor,
+        elevation: elevation,
+      ),
       child: Text(
         widget.title,
         style: TextStyle(
           color: theme.secondaryHeaderColor,
-          fontSize: 16,
+          fontSize: widget.fontSize,
           fontWeight: FontWeight.w600,
         ),
       ),
