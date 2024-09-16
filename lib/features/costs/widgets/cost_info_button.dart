054e3548f2d682a7946725725204cb8f7f36a5bc
sahland <sahland@mail.ru>
2024-09-16 21:15:18 +0300

    BakeBudget: refactoring costs

--- lib/features/costs/widgets/cost_info_button.dart
+++ lib/features/costs/widgets/cost_info_button.dart
@@ -3,13 +3,26 @@ import 'package:flutter/material.dart';
 class CostInfoButton extends StatefulWidget {
   final String costName;
   final int costPrice;
+
+  final double fontSize;
+  final double height;
+  final double width;
+  final double verticalPadding;
+  final double horizontalPadding;
+
   final VoidCallback? onPressed;
 
-  const CostInfoButton(
-      {super.key,
-      required this.costName,
-      required this.costPrice,
-      this.onPressed});
+  const CostInfoButton({
+    required this.costName,
+    required this.costPrice,
+    this.fontSize = 16,
+    this.height = 60,
+    this.width = double.infinity,
+    this.verticalPadding = 5,
+    this.horizontalPadding = 10,
+    this.onPressed,
+    super.key,
+  });
 
   @override
   State<CostInfoButton> createState() => _CostInfoButtonState();
@@ -18,51 +31,56 @@ class CostInfoButton extends StatefulWidget {
 class _CostInfoButtonState extends State<CostInfoButton> {
   @override
   Widget build(BuildContext context) {
+    const elevation = 10.0;
+
+    final costPriceTitle = '${widget.costPrice} руб.';
     final theme = Theme.of(context);
 
     return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 5,
-        horizontal: 10,
+      padding: EdgeInsets.symmetric(
+        vertical: widget.verticalPadding,
+        horizontal: widget.horizontalPadding,
       ),
       child: ElevatedButton(
         onPressed: widget.onPressed ?? () {},
         style: ElevatedButton.styleFrom(
           backgroundColor: theme.dialogBackgroundColor,
-          elevation: 10,
-          minimumSize: const Size(
-            double.infinity,
-            60,
+          elevation: elevation,
+          minimumSize: Size(
+            widget.width,
+            widget.height,
           ),
         ),
         child: Row(
-          children: [_costName(theme), const Spacer(), _costPrice(theme)],
+          children: [
+            _costInfoText(
+              theme,
+              widget.costName,
+              widget.fontSize,
+            ),
+            const Spacer(),
+            _costInfoText(
+              theme,
+              costPriceTitle,
+              widget.fontSize,
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Text _costName(ThemeData theme) {
+  Text _costInfoText(ThemeData theme, String title, double fontSize) {
+    const maxLines = 1;
+
     return Text(
       widget.costName,
       style: TextStyle(
         color: theme.secondaryHeaderColor,
-        fontSize: 16,
+        fontSize: fontSize,
       ),
       overflow: TextOverflow.ellipsis,
-      maxLines: 1,
-    );
-  }
-
-  Text _costPrice(ThemeData theme) {
-    return Text(
-      '${widget.costPrice} руб.',
-      style: TextStyle(
-        color: theme.secondaryHeaderColor,
-        fontSize: 16,
-      ),
-      overflow: TextOverflow.ellipsis,
-      maxLines: 1,
+      maxLines: maxLines,
     );
   }
 }
