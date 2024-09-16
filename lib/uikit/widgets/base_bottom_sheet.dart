081b30467a778aeaf0c936e9db4374c9888f68da
sahland <sahland@mail.ru>
2024-09-16 21:11:06 +0300

    BakeBudget: refactoring base bottom sheet widget

--- lib/uikit/widgets/base_bottom_sheet.dart
+++ lib/uikit/widgets/base_bottom_sheet.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class BaseBottomSheet extends StatelessWidget {
-  const BaseBottomSheet({
-    super.key,
-    required this.child,
-  });
-
   final Widget child;
+  final double borderRadius;
+  final double padding;
+  final double margin;
+
+  const BaseBottomSheet({
+    required this.child,
+    required this.borderRadius,
+    required this.padding,
+    required this.margin,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +20,11 @@ class BaseBottomSheet extends StatelessWidget {
 
     return Container(
       width: double.infinity,
-      margin: const EdgeInsets.symmetric(horizontal: 10).copyWith(bottom: 10),
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      //height: 74,
+      margin: EdgeInsets.symmetric(horizontal: margin).copyWith(bottom: margin),
+      padding: EdgeInsets.symmetric(horizontal: padding),
       decoration: BoxDecoration(
           color: theme.dialogBackgroundColor,
-          borderRadius: BorderRadius.circular(14)),
+          borderRadius: BorderRadius.circular(borderRadius)),
       child: SizedBox(
         width: double.infinity,
         child: child,
