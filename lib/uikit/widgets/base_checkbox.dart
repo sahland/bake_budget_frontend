127b7770992746d587464dc1731e93374c29e128
sahland <sahland@mail.ru>
2024-09-16 21:09:46 +0300

    BakeBudget: add base checkbox widget

---
+++ lib/uikit/widgets/base_checkbox.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class BaseCheckbox extends StatefulWidget {
+  final String title;
+  final int index;
+  final double fontSize;
+
+  const BaseCheckbox({
+    required this.title,
+    required this.index,
+    required this.fontSize,
+    super.key,
+  });
+
+  @override
+  State<BaseCheckbox> createState() => _BaseCheckboxState();
+}
+
+class _BaseCheckboxState extends State<BaseCheckbox> {
+  @override
+  Widget build(BuildContext context) {
+    int _selectedValue = 0;
+
+    const checkboxBorderRadius = 10.0;
+    final theme = Theme.of(context);
+
+    return CheckboxListTile(
+      title: Text(
+        widget.title,
+        style: TextStyle(
+          color: theme.secondaryHeaderColor,
+          fontSize: widget.fontSize,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      checkboxShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+        checkboxBorderRadius,
+      )),
+      value: _selectedValue == widget.index,
+      onChanged: (value) {
+        setState(() {
+          if (value!) {
+            _selectedValue = widget.index;
+          }
+        });
+      },
+    );
+  }
+}
