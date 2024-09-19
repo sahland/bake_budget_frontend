31bdd7a13598ff64685a5fd4c8d494661a088d47
sahland <sahland@mail.ru>
2024-09-19 21:05:27 +0300

    BakeBudget: refactoring product widget

--- lib/features/product/widgets/new_product_info.dart
+++ lib/features/product/widgets/new_product_info.dart
@@ -6,11 +6,23 @@ class NewProductInfo extends StatefulWidget {
   final String weight;
   final String imagePath;
 
+  final double width;
+  final double height;
+  final double verticalPadding;
+  final double horizontalPadding;
+
+  final VoidCallback? onTapImage;
+
   const NewProductInfo({
-    super.key,
     required this.title,
     required this.weight,
     required this.imagePath,
+    this.width = 130,
+    this.height = 130,
+    this.verticalPadding = 0,
+    this.horizontalPadding = 5,
+    this.onTapImage,
+    super.key,
   });
 
   @override
@@ -20,15 +32,17 @@ class NewProductInfo extends StatefulWidget {
 class _NewProductInfoState extends State<NewProductInfo> {
   @override
   Widget build(BuildContext context) {
+    const sizedBoxWidth = 16.0;
+
     final theme = Theme.of(context);
 
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+      padding: EdgeInsets.symmetric(vertical: widget.verticalPadding, horizontal: widget.horizontalPadding,),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _productImage(theme),
-          const SizedBox(width: 16),
+          const SizedBox(width: sizedBoxWidth,),
           _productFields(),
         ],
       ),
@@ -48,25 +62,28 @@ class _NewProductInfoState extends State<NewProductInfo> {
   }
 
   Widget _productImage(ThemeData theme) {
+    const sizedBoxHeight = 11.0;
+    const borderRadius = 8.0;
+
     return Column(
       children: [
         const SizedBox(
-          height: 11,
+          height: sizedBoxHeight,
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: widget.onTapImage ?? () {},
           child: Container(
-            width: 130,
-            height: 130,
+            width: widget.width,
+            height: widget.height,
             decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              borderRadius: const BorderRadius.all(Radius.circular(borderRadius),),
               color: theme.dialogBackgroundColor,
               boxShadow: [
                 _shadowButton(),
               ],
             ),
             child: ClipRRect(
-              borderRadius: BorderRadius.circular(8),
+              borderRadius: BorderRadius.circular(borderRadius,),
               child: Image.network(
                 widget.imagePath,
                 fit: BoxFit.cover,
@@ -79,11 +96,17 @@ class _NewProductInfoState extends State<NewProductInfo> {
   }
 
   BoxShadow _shadowButton() {
+    const withOpacity = 0.3;
+    const spreadRadius = 2.0;
+    const blurRadius = 8.0;
+    const offsetDx = 0.0;
+    const offsetDy = 3.0;
+
     return BoxShadow(
-      color: Colors.black.withOpacity(0.3),
-      spreadRadius: 2,
-      blurRadius: 8,
-      offset: const Offset(0, 3),
+      color: Colors.black.withOpacity(withOpacity,),
+      spreadRadius: spreadRadius,
+      blurRadius: blurRadius,
+      offset: const Offset(offsetDx, offsetDy),
     );
   }
 }
