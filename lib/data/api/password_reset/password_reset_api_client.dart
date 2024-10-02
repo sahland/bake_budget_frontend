e0ad2a44a7f28debb5e879fdc43e5395c1656192
SaHland <sahland@mail.ru>
2024-10-03 02:29:47 +0300

    BakeBudget: add password reset api client

---
+++ lib/data/api/password_reset/password_reset_api_client.dart
@@ -0,0 +1,36 @@
+import 'package:bake_budget_frontend/domain/domain.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'password_reset_api_client.g.dart';
+
+@RestApi(baseUrl: '')
+abstract class PasswordResetApiClient {
+  factory PasswordResetApiClient(
+    Dio dio,
+    {String baseUrl}
+  ) = _PasswordResetApiClient;
+
+  factory PasswordResetApiClient.create({String? apiUrl}) {
+    final dio = Dio();
+    if (apiUrl != null) {
+      return PasswordResetApiClient(dio, baseUrl: apiUrl);
+    }
+    return PasswordResetApiClient(dio);
+  }
+
+  @PUT('/api/password/setNew')
+  Future<void> updateOrder(
+    @Body() ResetPasswordModel resetPassword,
+  );
+
+  @POST('/api/password/forgot')
+  Future<ResetPasswordModel> forgotPassword(
+    @Part(name: 'email') String email,
+  );
+
+  @GET('/api/password/approveChangeLink')
+  Future<OrdersResponseModel> getOrderByID(
+    @Query('token') String token,
+  );
+}
---
+++ lib/data/api/password_reset/password_reset_api_client.g.dart
@@ -0,0 +1,149 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'password_reset_api_client.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element
+
+class _PasswordResetApiClient implements PasswordResetApiClient {
+  _PasswordResetApiClient(
+    this._dio, {
+    this.baseUrl,
+    this.errorLogger,
+  });
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  final ParseErrorLogger? errorLogger;
+
+  @override
+  Future<void> updateOrder(ResetPasswordModel resetPassword) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(resetPassword.toJson());
+    final _options = _setStreamType<void>(Options(
+      method: 'PUT',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/api/password/setNew',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    await _dio.fetch<void>(_options);
+  }
+
+  @override
+  Future<ResetPasswordModel> forgotPassword(String email) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = FormData();
+    _data.fields.add(MapEntry(
+      'email',
+      email,
+    ));
+    final _options = _setStreamType<ResetPasswordModel>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/api/password/forgot',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ResetPasswordModel _value;
+    try {
+      _value = ResetPasswordModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<OrdersResponseModel> getOrderByID(String token) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'token': token};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<OrdersResponseModel>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/api/password/approveChangeLink',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late OrdersResponseModel _value;
+    try {
+      _value = OrdersResponseModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
